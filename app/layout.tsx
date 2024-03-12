3633962b148603464bfa194bb28a63eed7dd73b9
iamhectorsosa <hello@hectorsosa.me>
2024-03-12 11:57:12 +0100

    Init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Schibsted_Grotesk as FontSans } from "next/font/google";
+import { IBM_Plex_Mono as FontMono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Providers } from "@/components/Providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontMono = FontMono({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-mono",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +27,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased text-base",
+          fontSans.variable,
+          fontMono.variable
+        )}
+      >
+        <Providers>
+          <main className="min-h-dvh mx-auto px-4 py-32">{children}</main>
+        </Providers>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,132 @@
-import Image from "next/image";
+import {
+  Blockquote,
+  Code,
+  Heading,
+  Kbd,
+  Link,
+  Text,
+} from "@/components/ui/typography";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flow max-w-4xl mx-auto">
+      <div>
+        <Heading>
+          The <em>principles</em> of the typographic craft are difficult to
+          master
+        </Heading>
+        <Text variant="lead" className="font-semibold">
+          The principles of the <Link href="/">typographic craft</Link> are
+          difficult to master
+        </Text>
+      </div>
+      <Text>
+        The goal of <Link href="/">typography</Link> is to relate font size,
+        line height, and line width in a proportional way that maximizes beauty
+        and makes reading easier and more pleasant.{" "}
+        <strong>
+          The question is: What proportion(s) will give us the best results?
+        </strong>{" "}
+        The golden ratio <Code>console.log(GOLDEN_RATIO)</Code> is often
+        observed in nature where beauty and utility intersect; perhaps we can
+        use this <em>“divine”</em> proportion to enhance these attributes in our
+        typography.
+      </Text>
+      <div>
+        <Heading as="h2" className="text-3xl">
+          What <em>else</em> can we expect?
+        </Heading>
+        <Blockquote>
+          Perfect typography is certainly the most elusive of all arts.
+          Sculpture in stone alone comes near it in obstinacy.
+        </Blockquote>
+      </div>
+      <div>
+        <Text>
+          Press <Kbd>⌘ C</Kbd> to show/hide the Theme Panel, or press{" "}
+          <Kbd>⌘ D</Kbd> to toggle dark mode.
+        </Text>
+        <Text variant="muted">Some restrictions may apply</Text>
+      </div>
+      <div>
+        <Heading as="h2" className="text-3xl">
+          Heading
+        </Heading>
+        <div>
+          <Heading>Heading 1</Heading>
+          <Heading as="h2">Heading 2</Heading>
+          <Heading as="h3">Heading 3</Heading>
+          <Heading as="h4">Heading 4</Heading>
+          <Heading as="h5">Heading 5</Heading>
+          <Heading as="h6">Heading 6</Heading>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <Heading as="h2" className="text-3xl">
+          Text
+        </Heading>
+        <div>
+          <Text>The quick brown fox jumps over the lazy dog.</Text>
+          <Text variant="large">
+            The quick brown fox jumps over the lazy dog.
+          </Text>
+          <Text variant="lead">
+            The quick brown fox jumps over the lazy dog.
+          </Text>
+          <Text variant="muted">
+            The quick brown fox jumps over the lazy dog.
+          </Text>
+          <Text variant="small">
+            The quick brown fox jumps over the lazy dog.
+          </Text>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <Heading as="h2" className="text-3xl">
+          Blockquote
+        </Heading>
+        <div>
+          <Blockquote>
+            Perfect typography is certainly the most elusive of all arts.
+            Sculpture in stone alone comes near it in obstinacy.
+          </Blockquote>
+          <Blockquote variant="subtle">
+            Perfect typography is certainly the most elusive of all arts.
+            Sculpture in stone alone comes near it in obstinacy.
+          </Blockquote>
+        </div>
       </div>
-    </main>
+      <div>
+        <Heading as="h2" className="text-3xl">
+          Code
+        </Heading>
+        <div className="*:block">
+          <Code variant="solid">console.log(&quot;Hello world&quot;)</Code>
+          <Code>console.log(&quot;Hello world&quot;)</Code>
+          <Code variant="ghost">console.log(&quot;Hello world&quot;)</Code>
+        </div>
+      </div>
+      <div>
+        <Heading as="h2" className="text-3xl">
+          Link
+        </Heading>
+        <div className="*:block">
+          <Link href="/">Learn more</Link>
+          <Link href="/" variant="subtle">
+            Learn more
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Heading as="h2" className="text-3xl">
+          Kbd
+        </Heading>
+        <div className="*:block">
+          <Kbd>⌘ C</Kbd>
+          <Kbd>⌘ C</Kbd>
+          <Kbd>⌘ C</Kbd>
+        </div>
+      </div>
+    </div>
   );
 }
